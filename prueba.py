b0af27f3a24df1870ee6cad9c551b42d95e73f68
marcosbeccar <beccarvarelam@udesa.edu.ar>
2023-09-05 21:15:36 -0300

    arregle el diccionario de valores

--- prueba.py
+++ prueba.py
@@ -1,58 +1,36 @@
 import random
 
 # Mazo
-mazo_completo = ["E1", "E2", "E3", "E4", "E5", "E6", "E7", "E10", "E11", "E12",
+mazo_cartas = ["E1", "E2", "E3", "E4", "E5", "E6", "E7", "E10", "E11", "E12",
                 "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B10", "B11", "B12",
                 "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C10", "C11", "C12",
                 "O1", "O2", "O3", "O4", "O5", "O6", "O7", "O10", "O11", "O12"]
 
-# Mazo + valores.
-valores_cartas = {
+valores_mazo_general = {
     "E1": 1, "E2": 2, "E3": 3, "E4": 4, "E5": 5, "E6": 6, "E7": 7, "E10": 0, "E11": 0, "E12": 0,
     "B1": 1, "B2": 2, "B3": 3, "B4": 4, "B5": 5, "B6": 6, "B7": 7, "B10": 0, "B11": 0, "B12": 0,
     "C1": 1, "C2": 2, "C3": 3, "C4": 4, "C5": 5, "C6": 6, "C7": 7, "C10": 0, "C11": 0, "C12": 0,
     "O1": 1, "O2": 2, "O3": 3, "O4": 4, "O5": 5, "O6": 6, "O7": 7, "O10": 0, "O11": 0, "O12": 0
 }
 
-# Iniciar mazos del jugador y de la computadora??
-cartas_jugador = []
-cartas_computadora = []
+mazo_jugador= random.sample(mazo_cartas, 3) #creo el mazo del jugador y elimino las cartas del mazo principal
+for carta_usada in mazo_jugador:
+    mazo_cartas.remove(carta_usada)
 
-# Asignación de cartas al user.
-while len(cartas_jugador) < 3:
-    carta_index = random.randint(0, len(mazo_completo))
-    carta = mazo_completo.pop(carta_index) #
-    cartas_jugador += [carta]
+mazo_comp= random.sample (mazo_cartas, 3) #creo el mazo de la computadora y elimino las cartas del mazo principal
+for carta_usada in mazo_comp:
+    mazo_cartas.remove(carta_usada)
 
-# Asignación de cartas a la computadora
-while len(cartas_computadora) < 3:
-    carta_index = random.randint(0, len(mazo_completo))
-    carta =mazo_completo.pop(carta_index)
-    cartas_computadora += [carta]
+#calculo valor del mazo
+valor_jugador= sum(valores_mazo_general[valor] #indico de donde tiene que sacar el valor
+                   for valor in mazo_jugador)  #indico que cartas aplican a la suma
 
-# Valor total de cada mazo.
-valor_total_jugador = sum(valores_cartas[carta] for carta in cartas_jugador)
-valor_total_computadora = sum(valores_cartas[carta] for carta in cartas_computadora)
-
-print("Cartas del jugador:", cartas_jugador)
-print("Valor total del mazo del jugador:", valor_total_jugador)
-
-print("Cartas de la computadora:", cartas_computadora)
-print("Valor total del mazo de la computadora:", valor_total_computadora)
+valor_comp= sum (valores_mazo_general[valor]   #indico de donde tiene que sacar el valor
+                 for valor in mazo_comp)       #indico que cartas aplican a la suma
 
 
-#Intento de calculo de envido 
-igual_palo=0
-if 'E' in cartas_computadora:
-    igual_palo+=1
-elif 'O' in cartas_computadora:
-    igual_palo+=1
-elif 'B' in cartas_computadora:
-    igual_palo+=1
-elif 'C' in cartas_computadora:
-    igual_palo+=1
-    
-if igual_palo <2:
-    igual=True
-    
-print (igual)
+
+print (mazo_jugador)
+print (mazo_comp)
+print ('valor jugador', valor_jugador)
+print ('valor_comp', valor_comp)
