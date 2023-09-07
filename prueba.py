21f24b788329c41527ddb3b477cfb1ce7bbf6aa5
marcosbeccar <beccarvarelam@udesa.edu.ar>
2023-09-07 17:56:36 -0300

    aksjbjdaksj

--- prueba.py
+++ prueba.py
@@ -1,6 +1,6 @@
 import random
 
-# Mazo
+# MAZO DE LAS CARTAS
 mazo_cartas = ["E1", "E2", "E3", "E4", "E5", "E6", "E7", "E10", "E11", "E12",
                 "B1", "B2", "B3", "B4", "B5", "B6", "B7", "B10", "B11", "B12",
                 "C1", "C2", "C3", "C4", "C5", "C6", "C7", "C10", "C11", "C12",
@@ -12,6 +12,12 @@ valores_mazo_general = {
     "C1": 1, "C2": 2, "C3": 3, "C4": 4, "C5": 5, "C6": 6, "C7": 7, "C10": 0, "C11": 0, "C12": 0,
     "O1": 1, "O2": 2, "O3": 3, "O4": 4, "O5": 5, "O6": 6, "O7": 7, "O10": 0, "O11": 0, "O12": 0
 }
+
+
+
+
+
+#REPARTIDOR DE CARTAS
 #manos = random.sample(mazo_cartas , 6)
 mazo_jugador= random.sample(mazo_cartas, 3) #creo el mazo del jugador y elimino las cartas del mazo principal
 for carta_usada in mazo_jugador:
@@ -21,6 +27,8 @@ mazo_comp= random.sample (mazo_cartas, 3) #creo el mazo de la computadora y elim
 for carta_usada in mazo_comp:
     mazo_cartas.remove(carta_usada)
 
+
+#ACCESO AL PALO DE LA CARTA
 palo1=mazo_jugador[0][0]
 palo2=mazo_jugador[1][0]
 palo3=mazo_jugador[2][0]
@@ -30,21 +38,100 @@ palo2c=mazo_comp[1][0]
 palo3c=mazo_comp[2][0]
 
 
+#ACCESO AL VALOR DE LAS CARTAS
+valor_carta_1_jugador=valores_mazo_general[mazo_jugador[0]]
+valor_carta_2_jugador = valores_mazo_general[mazo_jugador[1]]
+valor_carta_3_jugador = valores_mazo_general[mazo_jugador[2]]
+
+valor_carta_1_computadora = valores_mazo_general[mazo_comp[0]]
+valor_carta_2_computadora = valores_mazo_general[mazo_comp[1]]
+valor_carta_3_computadora = valores_mazo_general[mazo_comp[2]]
+
+#lista con valores
+lista_valor_jugador=[valor_carta_1_jugador, valor_carta_2_jugador, valor_carta_3_jugador]
+lista_valor_computadora=[valor_carta_1_computadora, valor_carta_2_computadora, valor_carta_3_computadora]
+
+
+#mazo_jugador
+puntos_jugador=0
+if palo1==palo2==palo3:
+    lista= sorted(lista_valor_jugador)
+    puntos_jugador=lista[1]+lista[2]+20
+elif palo1==palo2 or palo1==palo3 or palo2==palo3:
+    if palo1==palo2:
+        puntos_jugador=valor_carta_1_jugador + valor_carta_2_jugador +20
+    elif palo1==palo3:
+        puntos_jugador=valor_carta_1_jugador + valor_carta_3_jugador +20
+    elif palo2==palo3:
+        puntos_jugador=valor_carta_2_jugador + valor_carta_3_jugador +20
+else:
+    puntos_jugador=max(lista_valor_jugador)
+    
+
+#mazo computadora
+puntos_computadora=0
+if palo1c==palo2c==palo3c:
+    lista= sorted(lista_valor_computadora)
+    puntos_computadora=lista[1]+lista[2]+20
+elif palo1c==palo2c or palo1c==palo3c or palo2c==palo3c:
+    if palo1c==palo2c:
+        puntos_computadora=valor_carta_1_computadora + valor_carta_2_computadora +20
+    elif palo1c==palo3c:
+        puntos_computadora=valor_carta_1_computadora + valor_carta_3_computadora +20
+    elif palo2c==palo3c:
+        puntos_computadora=valor_carta_2_computadora + valor_carta_3_computadora +20
+else:
+    puntos_computadora=max(lista_valor_computadora)
+    
+print(puntos_jugador)
+print(puntos_computadora)
+
+#inicio de variable para calculo de puntos totales
+puntos_total_jugador=0
+puntos_total_computadora=0
+
+#canto de envido
+quien_mano=['mano_jugador', 'mano_comp']
+es_mano= random.choice(quien_mano)
 
 
 
+#presentación
+print ('----Bienvenido al simulador de envido----')
+print (f'Sus cartas son: {mazo_jugador}')
+    
+    
+while puntos_total_jugador <15 or puntos_total_computadora <15:
+    
+    if es_mano==['mano_jugador']:
+        print('Usted es mano')
+        eleccion=input('envido, no envido: ')
+        if eleccion=='envido':
+            if puntos_computadora <20:
+                print ('computadora: no quiero')
+            elif puntos_computadora >=20:
+                print ('envido')
+            else:
+                print ('quiero')
+        elif eleccion== 'no envido':
+            if puntos_computadora <20:
+                print ('computadora: no canta envido')
+            elif puntos_computadora >=20:
+                print ('computadora: envido')
+                eleccion_segunda=input('envido, no quiero: ')
+                if eleccion_segunda== 'envido':
+                    gkj
+                elif eleccion_segunda== 'no quiero':
+                    aaa
+    
+    
+#caso: jugador es mano
 
 
-#calculo valor del mazo
-valor_jugador= sum(valores_mazo_general[valor] #indico de donde tiene que sacar el valor
-                   for valor in mazo_jugador)  #indico que cartas aplican a la suma
-
-valor_comp= sum (valores_mazo_general[valor]   #indico de donde tiene que sacar el valor
-                 for valor in mazo_comp)       #indico que cartas aplican a la suma
 
 
 
 print (mazo_jugador)
 print (mazo_comp)
-print ('valor jugador:', valor_jugador)
-print ('valor_comp:', valor_comp)
+#print ('valor jugador:', valor_jugador)
+#print ('valor_comp:', valor_comp)
